6bc4bd1331e83defee4b23a968317b3e3b75693a
Hrishikesh Koundinya <63454195+Hrishikesh015@users.noreply.github.com>
2022-11-14 13:01:19 +0530

    final commit

--- config/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const connectDB = async() => {
-    await mongoose.connect('mongodb+srv://admin:Hrishi07@cluster0.3b09hac.mongodb.net/?retryWrites=true&w=majority')
-    console.log('MongoDb connected')
-}
-
-module.exports = connectDB;
