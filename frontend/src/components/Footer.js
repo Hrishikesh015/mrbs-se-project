9bb58dbc8cc3953e269bc17d2e8bc32425fc2bbd
Chethan <47030235+ChethanDatta@users.noreply.github.com>
2022-11-14 17:14:15 +0530

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
             <Container>
                 <Row>
                     <Col className="text-center py-3">
-                        Copyright &copy; {new Date().getFullYear()}
+                       THANK YOU
                     </Col>
                 </Row>
             </Container>
